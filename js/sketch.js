ca1455440dc97729fe186a798e4c8c2f2c117fa4
Ryan <ryan.d.elliott@gmail.com>
2017-09-25 14:01:48 -0600

    Add boilerplate code

---
+++ js/sketch.js
@@ -0,0 +1,15 @@
+// Canvas manipulation code goes here
+/* 2 main functions: 
+	setup() - runs on load 
+	draw() - runs every frame
+*/
+
+function setup(){
+	console.log('Running setup()');
+
+
+}
+
+function draw(){
+	
+}
